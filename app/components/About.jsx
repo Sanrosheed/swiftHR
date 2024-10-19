a40475211d4f10fb0faf6343090b4fc663fecffa
sanrosheed <sanrosheed01@gmail.com>
2024-10-19 11:33:57 +0100

    initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center lg:px-32 px-5 bg-backgroundColor">
+      <h1 className="font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">
+        About Us
+      </h1>
+
+      <div className="flex flex-col lg:flex-row items-center gap-5">
+        <div className="w-full lg:w-2/4">
+          <img
+            className="rounded-lg"
+            src="https://cdn.hashnode.com/res/hashnode/image/upload/v1729330650987/dbf6cd3b-7bae-4709-8d1a-a88d2bf26550.jpeg"
+            alt="img"
+          />
+        </div>
+        <div className="w-full lg:w-2/4 p-4 space-y-3">
+          <h2 className="font-semibold text-3xl">
+            What Makes Our organization Special?
+          </h2>
+          <p>
+            We are a leading organization dedicated to helping businesses
+            achieve their HR goals and find exceptional talents across all
+            fields. We have a track record of excellence and reliability as we
+            have offered services to a variety of high tech companies such as
+            Gooogle, Amazon, Facebook and others.
+          </p>
+          <p>
+            A bite gives you an elysian feeling while keeping you elated and
+            nourished
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/components/Footer.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gradient-to-r from-gray-900 to-blue-600 text-white rounded-t-3xl mt-8 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
+        <div className="w-full md:w-1/4">
+          <h1 className="font-semibold text-xl pb-4">SwiftHR</h1>
+          <p className="text-sm">
+            We are a leading organization dedicated to helping businesses
+            achieve their HR goals and find exceptional talents across all
+            fields.
+          </p>
+        </div>
+        <div>
+          <h1 className="font-medium text-xl pb-4 pt-5 md:pt-0">Links</h1>
+          <nav className="flex flex-col gap-2">
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              About Us
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Our Services
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Reviews
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className="font-medium text-xl pb-4 pt-5 md:pt-0">
+            Informations
+          </h1>
+          <nav className="flex flex-col gap-2">
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Careers
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Blog
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Help & FAQs
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className="font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
+          <nav className="flex flex-col gap-2">
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              swiftHR@gmail.com
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              +234 777 888 999
+            </a>
+            <a
+              className="hover:text-backgroundColor transition-all cursor-pointer"
+              href="/"
+            >
+              Social Media
+            </a>
+          </nav>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-center py-4">
+          @copyright developed by{" "}
+          <span className="text-backgroundColor">Sanrosheed</span> | All rights
+          reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
